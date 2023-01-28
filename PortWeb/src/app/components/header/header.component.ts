c29d949a8cbd5c98b6481ec6385496742ffe9b5c
Sofia <116586560+sofisciangu@users.noreply.github.com>
2023-01-28 17:54:42 -0300

    css 28/1

--- PortWeb/src/app/components/header/header.component.ts
+++ PortWeb/src/app/components/header/header.component.ts
@@ -1,20 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { PorfolioService } from 'src/app/servicios/porfolio.service';
+import { Component} from '@angular/core'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent{
 
-  constructor (private datosPorfolio: PorfolioService) {
-    
-  }
-
-  ngOnInit(): void {
-    this.datosPorfolio.obtenerDatos();
-    
-  }
-  }
+}
 
