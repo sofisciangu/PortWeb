4d313d539761b70e7727dc28e6e385bf615234ef
Sofia <116586560+sofisciangu@users.noreply.github.com>
2023-03-09 22:20:14 -0300

    btn log in

--- PortWeb/src/app/app.module.ts
+++ PortWeb/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -17,6 +16,7 @@ import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
+    LogoAPComponent,
     AppComponent,
     BannerComponent,
     AcercaDeComponent,
