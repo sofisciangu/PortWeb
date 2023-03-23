16b250b2f96314bc2fb566e1cd9a7f59ff333dfc
Sofia <116586560+sofisciangu@users.noreply.github.com>
2023-03-22 22:17:54 -0300

    ver

--- PortWeb/src/app/components/logo-ap/logo-ap.component.ts
+++ PortWeb/src/app/components/logo-ap/logo-ap.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TokenService } from 'src/app/servicios/token.service';
+
 
 @Component({
   selector: 'app-logo-ap',
@@ -8,9 +10,22 @@ import { Router } from '@angular/router';
 })
 
 export class LogoAPComponent implements OnInit {
-  constructor(private router:Router){}
+  isLogged = false;
+  constructor(private router:Router, private tokenService: TokenService){}
+  
 
-  ngOnInit():void{}
+  ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged=true;
+    }else{
+      this.isLogged = false;
+    }
+  }
+
+  onLogOut():void{
+    this.tokenService.logOut();
+    window.location.reload();
+  }
 
   login(){
     this.router.navigate(['/login'])
